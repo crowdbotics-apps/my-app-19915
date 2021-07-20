b55cadd40cab893377f0cb3c073b2f7849a3854a
Saad bin abid <saad.abid@crowdbotics.com>
2021-07-20 02:50:13 -0700

    fixes in projects

--- android/app/src/main/java/com/my_app_19915/generated/BasePackageList.java
+++ android/app/src/main/java/com/my_app_19915/generated/BasePackageList.java
@@ -2,15 +2,15 @@ package com.my_app_19915.generated;
 
 import java.util.Arrays;
 import java.util.List;
-import org.unimodules.core.interfaces.Package;
+//import org.unimodules.core.interfaces.Package;
 
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.constants.ConstantsPackage(),
-        new expo.modules.filesystem.FileSystemPackage(),
-        new expo.modules.imageloader.ImageLoaderPackage(),
-        new expo.modules.permissions.PermissionsPackage()
+        // new expo.modules.constants.ConstantsPackage(),
+        // new expo.modules.filesystem.FileSystemPackage(),
+        // new expo.modules.imageloader.ImageLoaderPackage(),
+        // new expo.modules.permissions.PermissionsPackage()
     );
   }
 }
